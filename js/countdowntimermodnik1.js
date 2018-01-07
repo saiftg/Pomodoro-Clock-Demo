408ec2b847b404c2f72bd74a2ef6ec515e1c5e83
Saif Mahmud <saifalmahmud@gmail.com>
2018-01-07 13:26:54 -0500

    Add files via upload
    Cleanup

--- js/countdowntimermodnik1.js
+++ js/countdowntimermodnik1.js
@@ -1,40 +1,39 @@
 
-console.log("test test")
-//setting up global variables
+console.log("test")
 var workLength = 25;
 var playLength = 5;
 // var alarm = new Audio('http://www.orangefreesounds.com/wp-content/uploads/2016/06/Ringing-clock.mp3?_=1');
 var alarm = new Audio('sound/timesup.mp3')
 var loop = 0;
-var longRest = 1;
+var longRest =1;
 var longBreak = 30;
 var jokeInt;
+var sliderWait = false;
 var timeWorked = [];
-var timeOnBreak = []; // global variable for jokes
-//end of setting up global variables
-$('#reset').hide(); //leaves only start button in the beginning
+var timeOnBreak = [];
+
+
+$('#reset').hide(); //leaves only start button on start
 // document.querySelector("#myCard").classList.toggle("flip")
 // $("#myCard").click(function(){          //that was a test button for flipping screen
 //     $(".flipper").toggleClass("flip");
 // });
 
 
+// Make Logo bounce upon click
 $('.logo').click(function(){
   $('.logo').effect( "bounce", { times: 3 }, "slow" );
 });
-
 $('#worktime').hide();
 $('#breaktime').hide();
+
 //sliding intructions start
 $('.slideTogglebox').hide();
-$('#slideToggle').mouseover(function(){
-    $(this).css("color","blue")
-    $('.slideTogglebox').slideDown();
+$('#slideToggle').click(function(){
+    // $(this).css("color","blue")
+    $('.slideTogglebox').slideToggle();
 })
-$('#slideToggle').mouseout(function(){
-    $(this).css("color","#F0EAD6")
-    $('.slideTogglebox').slideUp();
-})
+
 //sliding instructions end
 
 //sliding footer
@@ -42,11 +41,11 @@ $('#slideToggle').mouseout(function(){
 $('#teamTogglebox').hide();
 $('#team').mouseover(function(){
     $(this).css("color","blue")
-    $('#teamTogglebox').slideDown();
+    $('#teamTogglebox').slideToggle();
 })
 $('#team').mouseout(function(){
     $(this).css("color","#F0EAD6")
-    $('#teamTogglebox').slideUp();
+    $('#teamTogglebox').slideToggle();
 })
 //sliding instructions for footer
 
@@ -58,11 +57,11 @@ $('#team').mouseout(function(){
     if (workLength > 90){ // logic to limit to 2 digits
         workLength = 95
     }else{
-    $('.minutes').html(workLength);
+    $('.minutes').text(workLength);
     $('.minutes-interval').text(workLength);// using html and text interchangebly
     }                                       // seems to be no difference
-    $('.minutes').html(workLength);
-    $('.minutes-interval').html(workLength);
+    $('.minutes').text(workLength);
+    $('.minutes-interval').text(workLength);
   });
 
 
@@ -92,9 +91,9 @@ $('#team').mouseout(function(){
     playLength -= 5;
     if (playLength < 10){ //logic to stay with 2 digits
       playLength = 5
-      $("#playTimer").html('0' + playLength);
+      $("#playTimer").text('0' + playLength);
     }else{
-      $("#playTimer").html(playLength)
+      $("#playTimer").text(playLength)
     } 
   });
 //end of listeners for buttons plus and minus
@@ -102,17 +101,14 @@ $('#team').mouseout(function(){
 function startTimer() {    //to start timer
   $('#start').fadeOut();
   $('#reset').fadeIn();
-  loop = 0;
   seconds = 0;
-  countDown(workLength, seconds, loop);
+  countDown(workLength, seconds);
 }
 
 function reset() {         //to reset timer
     clearInterval(countInt);
     workLength = 25;
     playLength = 5;
-    longRest = 1;
-    loop = 0;
     $('.minutes').html(workLength); //resets minutes and seconds on main timer
     $('.seconds').html('00');
     $('.minutes-interval').html(workLength); //resets minutes on user input
@@ -120,52 +116,75 @@ function reset() {         //to reset timer
     $('#start').fadeIn();
     $('#reset').fadeOut();
     $('#current-session').html('Work Session');
-    jokeInt = setTimeout(getJoke, 50000000); 
-
+     clearInterval(jokeInt); 
     function getSum(total, num) {
     return total + num;
 }
       console.log("Total time worked is" + " " + timeWorked.reduce(getSum) + " " + "minutes");
       console.log("Total time spent on break is" + " " + timeOnBreak.reduce(getSum) + " " + "minutes");
-
-    $('#worktime').html("Time worked:" + " " + timeWorked.reduce(getSum) + " " + "minutes");
+   
+    $('#start').show();
+    $('#worktime').html("Time worked: " + " " + timeWorked.reduce(getSum) + " " + "minutes");
     $('#worktime').show();
     document.getElementById("worktime").disabled = true;
     $('#breaktime').show();
     document.getElementById("breaktime").disabled = true;
     $('#breaktime').html("Time on break: " + " " + timeOnBreak.reduce(getSum) + " " + "minutes");
     $('#reset').hide();
-    $('#start').show();
-
-
-
-
 }
 
-  
 
-
+// function countDown(minutes,seconds) {
+//  countInt = setInterval(function(){
+//     if (minutes == 0 && seconds == 0) {
+//         clearInterval(countInt);
+//         if (loop == 0) {
+//             time = playLength;
+//             timeOnBreak.push(playLength);
+//             loop += 1;
+//             $('#current-session').text('Rest Time');
+//             $(".flipper").toggleClass("flip");
+//         } else {
+//             time = workLength;
+//             loop -= 1;
+//             $(".flipper").toggleClass("flip");
+//             $('#current-session').text('Work Session');
+//         }
+    //       alarm.play();
+    //       $('.logo').effect( "bounce", { times: 3 }, "slow" );
+          
+    //   //var timeOut = setTimeout(explode, 2000);
+    //   countDown(time,0); // recursive timer call
+    //   } else if (seconds != 0) {
+    //       seconds -= 1;
+    //   } else if (seconds == 0) {
+    //       seconds = 59;
+    //       minutes -= 1;
+    //   }
+    //   var formattedMinutes = ("0" + minutes).slice(-2);//format m and s to take 2 digits
+    //   var formattedSeconds = ("0" + seconds).slice(-2); 
+    //   $('.minutes').html(formattedMinutes);
+    //   $('.seconds').html(formattedSeconds);
+        
+    // }, 10);
+// }   
 function getJoke(){
-// var url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback="
-    console.log('get joke - got called')
-    var url='https://api.icndb.com/jokes/random?exclude=[explicit]'
-    $.getJSON(url, function(result) {
-        $("#chuck-joke").html(result.value.joke);
-        console.log("new joke", result)
-    });
-    jokeInt = setTimeout(getJoke, 5000); 
+  console.log('get joke - get called')
+  var url='https://api.icndb.com/jokes/random?exclude=[explicit]'
+  $.getJSON(url, function(result){
+    $("#chuck-joke").html(result.value.joke);
+    console.log('new joke',result)
 
- //timer to call new joke after 5 seconds delay
-  }
-
+  });
+  jokeInt = setTimeout(getJoke,9000);
+}
 
 function countDown(minutes,seconds) { 
  countInt = setInterval(function(){
-
     if (minutes == 0 && seconds == 0) { //since minutes are not '0' it skips directly to the timer
         clearInterval(countInt);        
         if (loop == 0) {
-            if (longRest % 2 != 0 || longRest == 1){//this checks if this is short break or long break
+            if (longRest % 4 != 0 || longRest == 1){//this checks if this is short break or long break
               time = playLength;
               console.log('keeping short break')
               timeOnBreak.push(playLength);
@@ -212,9 +231,11 @@ function countDown(minutes,seconds) {
             $('#current-session').html('Work Session');
             console.log(timeWorked);
         }
-          alarm.play();
-      // var timeOut = setTimeout(explode, 2000);
-      countDown(time,0); // timer, recursive call
+        alarm.play();
+          $('.logo').effect( "bounce", { times: 3 }, "slow" );
+          
+      //var timeOut = setTimeout(explode, 2000);
+      countDown(time,0); // recursive timer call
       } else if (seconds != 0) {
           seconds -= 1;
       } else if (seconds == 0) {
@@ -227,11 +248,4 @@ function countDown(minutes,seconds) {
       $('.seconds').html(formattedSeconds);
         
     }, 10);
-}   
- 
-// function explode(){
-//    $("#current-session").hide( "explode", {pieces: 16 }, 2000 );
-//    // clearInterval(timeOut);
-//    return
-// }
-
+}
